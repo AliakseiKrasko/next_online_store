8700cd2a75bde5e2434b58d37b827be5a53dcaf1
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-10-03 09:34:57 +0300

    feat: added to framer-motion

--- src/components/slider/SlideTwo.tsx
+++ src/components/slider/SlideTwo.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 
-function SliderTwo() {
+function SlideTwo() {
   return (
     <div className="mb-10 md:mb-15 xl:mb-20 h-20 md:h-40 xl:h-50 w-full relative">
       <Image
@@ -8,10 +8,11 @@ function SliderTwo() {
         alt="Слайд"
         fill
         priority
+        unoptimized
         className="object-cover"
       />
     </div>
   );
 }
 
-export default SliderTwo;
+export default SlideTwo;
